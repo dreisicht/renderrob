860f0c716b9053b388e61a7b2187be60bfe8a45c
PeterBaintner <pb@dreisicht.net>
2020-07-06 11:18:26 +0200

    Render Rob 2.0 RC4

--- src/rr_gspread.py
+++ src/rr_gspread.py
@@ -4,16 +4,13 @@ import sys
 import pickle
 import webbrowser
 
-import google.oauth2.credentials
+from sty import fg, bg, ef, rs, Style, RgbBg
+import sty
 
+import google.oauth2.credentials
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from google.oauth2 import service_account
-from apiclient import errors
-
-
 from datetime import datetime
 
 SHEETNAME = 'Render Rob 2.0'
@@ -35,15 +32,13 @@ def print_info(ipt_str):
     print(time_current, ipt_str)
 
 
-# def get_sheet_name():
-#     current_path = os.path.dirname(
-#         os.path.realpath(__file__)).replace("\\", "/")+"/"
-#     try:
-#         f_ini = open(current_path + "user/sheetname.ini", "r")
-#         sheetname = f_ini.read()
-#         f_ini.close()
-#     except FileNotFoundError:
-#         sheetname = SHEETNAME
+def print_info_input(ipt_str):
+    bg.dark_blue = Style(
+        RgbBg(69, 129, 142))
+    time_current = "[{}]".format(
+        datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    input(bg.dark_blue + fg.white + time_current +
+          "[INFO] " + ipt_str + " press Enter to continue." + rs.all)
 
 
 def get_authenticated_service():
@@ -65,39 +60,6 @@ def get_authenticated_service():
     return build('sheets', 'v4', credentials=creds)
 
 
-def get_authenticated_service_drive():
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, (['https://www.googleapis.com/auth/drive.file']))
-
-    if os.path.exists('src/util/token.pickle'):
-        with open('src/util/token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-    else:
-        creds = flow.run_local_server()
-        with open('src/util/token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    return build('drive', 'v3', credentials=creds)
-
-
-def get_service_account_service():
-    secret_file = os.path.join(os.getcwd(), 'src/service_account.json')
-    creds = service_account.Credentials.from_service_account_file(
-        secret_file, scopes=SCOPES)
-    return build('sheets', 'v4', credentials=creds)
-
-
-def get_service_account_service_drive():
-    secret_file = os.path.join(os.getcwd(), 'src/service_account.json')
-    creds = service_account.Credentials.from_service_account_file(
-        secret_file, scopes=SCOPES)
-    return build('drive', 'v3', credentials=creds)
-
-
 def create_sheet(service):
     spreadsheet = {
         'properties': {
@@ -109,36 +71,10 @@ def create_sheet(service):
     print('Created Spreadsheet with ID: {0}'.format(
         spreadsheet.get('spreadsheetId')))
     webbrowser.open(
-        "https://docs.google.com/spreadsheets/d/" + TEMPLATE_ID)
-    webbrowser.open(
         "https://docs.google.com/spreadsheets/d/" + spreadsheet.get('spreadsheetId'))
-    return spreadsheet.get('spreadsheetId')
-
-
-def copy_sheet(service, sa_service):
-    template_id = "1prG29lIic0Qqc_fGq5sLdnDh1u8Nkd45MxdaCuRp1Rw"
-    new_spreadsheet_id = create_sheet(service)
-
-    dest_body = {
-        'destination_spreadsheet_id': new_spreadsheet_id,
-    }
-
-    jobs_sheet = sa_service.spreadsheets().sheets().copyTo(
-        spreadsheetId=template_id, sheetId=259211185, body=dest_body).execute()
-    jobs_sheet_id = jobs_sheet.get("sheetId")
-    renameSheet(service, new_spreadsheet_id, jobs_sheet_id, "jobs")
-
-    globals_sheet = sa_service.spreadsheets().sheets().copyTo(
-        spreadsheetId=template_id, sheetId=209628393, body=dest_body).execute()
-    globals_sheet_id = globals_sheet.get("sheetId")
-    renameSheet(service, new_spreadsheet_id, globals_sheet_id, "globals")
-
-    delete_sheet(service, new_spreadsheet_id, 0)
-
     webbrowser.open(
-        "https://docs.google.com/spreadsheets/d/" + new_spreadsheet_id)
-
-    return new_spreadsheet_id
+        "https://docs.google.com/spreadsheets/d/" + TEMPLATE_ID)
+    return spreadsheet.get('spreadsheetId')
 
 
 def batch(service, spreadsheetId, requests):
@@ -180,14 +116,6 @@ def delete_sheet(service, spreadsheetId, sheetId):
     }
     )
 
-# def get_sheet_id_by_name(service):
-#     spreadsheet = service.spreadsheets().get(
-#         spreadsheetId=SPREADSHEET_ID).execute()
-#     sheet_id = None
-#     for _sheet in spreadsheet['sheets']:
-#         if _sheet['properties']['title'] == sheet_name:
-#             sheet_id = _sheet['properties']['sheetId']
-
 
 def get_values(service, spreadsheetId, range):
     result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, majorDimension="ROWS",
@@ -205,87 +133,37 @@ def query_sheet():
         spreadsheetId = sheetcache.read()
         sheetcache.close()
     except FileNotFoundError:
-        service = get_authenticated_service()
-        spreadsheetId = create_sheet(service)
+        spreadsheetId = create_sheet(service_sheets)
         sheetcache = open("src/util/sheetcache", "x")
         sheetcache.write(spreadsheetId)
         sheetcache.close()
-        print_info(
-            "Created the Render Rob Spreadsheet. Please copy both sheets from the template sheet to the newly created one.")
-        sys.exit()
+        print_info_input(
+            "Created the Render Rob Spreadsheet. Please copy both sheets from the template sheet to the newly created one. If you copied them")
+        cleanup_sheet(service_sheets, spreadsheetId)
 
     jobs_values = get_values(service_sheets, spreadsheetId, "jobs!A1:V81")
     globals_values = get_values(service_sheets, spreadsheetId, "globals!A1:C7")
     return jobs_values, globals_values
 
 
-def query_files():
-    service_drive = get_authenticated_service_drive()
-    results = service_drive.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+def cleanup_sheet(service, spreadsheetId):
+    # get list of sheets
+    sheet_metadata = service.spreadsheets().get(
+        spreadsheetId=spreadsheetId).execute()
+    sheets = sheet_metadata.get('sheets', '')
 
-
-def print_permission(service, file_id, permission_id):
-    """Print information about the specified permission.
-
-    Args:
-      service: Drive API service instance.
-      file_id: ID of the file to print permission for.
-      permission_id: ID of the permission to print.
-    """
-    try:
-        permission = service.permissions().get(
-            fileId=file_id, permissionId=permission_id).execute()
-
-        print(permission)
-        # print('Role: %s' % permission['role'])
-        for additional_role in permission.get('additionalRole', []):
-            print('Additional role: %s' % additional_role)
-    except errors.HttpError as error:
-        print('An error occurred: %s' % error)
-
-
-def read_as_service_account():
-    # sh_service = get_authenticated_service()
-    dr_service = get_authenticated_service_drive()
-    sa_service = get_service_account_service_drive()
-    # newid = create_sheet(sa_service)
-    # copy_sheet(sh_service, sa_service)
-    id1 = "1prG29lIic0Qqc_fGq5sLdnDh1u8Nkd45MxdaCuRp1Rw"
-    id2 = "1_wxJIgni2Ee45WUZNE1XeqzchVRXmYXAwDPOxofIrIs"
-    # oa_newsheet_id = "1Qmpo6fB9IufZN_0Py5QiX-hTDNLI4_6TRWJJsdKIYng"
-    # sa_newsheet_id = create_sheet(sa_service)
-    
-    # print(get_values(sa_service, id, "A1:Z8"))
-    # dest_body = {
-    #     'destination_spreadsheet_id': sa_newsheet_id
-    # }
-
-    # jobs_sheet = sa_service.spreadsheets().sheets().copyTo(
-    #     spreadsheetId=id, sheetId=259211185, body=dest_body).execute()
-
-    # copy_file(sa_service, id, "Render Rob successfull copy")
-    sa_service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    # print_permission(sa_service, id1, "owners")
-    print_permission(sa_service, id2, "owners")
-    # print_permission(sa_service, id1, "")
-    print_permission(sa_service, id2, "")
-    # print_permission(dr_service, id1, "")
-    print_permission(dr_service, id2, "")
-    # sa_service.permissions().inert()
-
-
-def copy_file(service, origin_file_id, copy_title):
-    copied_file = {'title': copy_title}
-    return service.files().copy(fileId=origin_file_id, body=copied_file).execute()
+    for i in range(len(sheets)):
+        title = sheets[i].get("properties", {}).get("title")
+        sheet_id = sheets[i].get("properties", {}).get("sheetId")
+        print(title)
+        if "jobs" in title and title != "jobs":
+            renameSheet(service, spreadsheetId, sheet_id, "jobs")
+            title = "jobs"
+        elif "globals" in title and title != "globals":
+            renameSheet(service, spreadsheetId, sheet_id, "globals")
+            title = "globals"
+        elif title != "jobs" and title != "globals":
+            delete_sheet(service, spreadsheetId, sheet_id)
 
 
 if __name__ == "__main__":
