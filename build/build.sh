3bb863b982f8abd88be89d1e75d67100dcd9c7f4
PeterBaintner <pb@dreisicht.net>
2020-07-11 14:46:51 +0200

    updated linux build script

--- build/build.sh
+++ build/build.sh
@@ -5,12 +5,15 @@ pip3 install xlrd
 pip3 install gspread
 pip3 install oauth2client
 pip3 install sty
+pip3 install google-api-python-client
+pip3 install google
+pip3 install google_auth_oauthlib
 pip3 install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
 
 sudo apt install libxi6 libxrender1 libgl1-mesa-glx
 curl -o blender.tar.xz https://builder.blender.org/download/blender-2.83-e5ace51295b9-linux64.tar.xz
 tar -xvf blender.tar.xz
 
-python3 -m PyInstaller --onefile --icon /home/ubuntu/RenderRob/img/renderrob_icon.ico /home/ubuntu/RenderRob/src/rr_master.py
+python3 -m PyInstaller --hidden-import google-api-python-client --onefile --icon /home/ubuntu/RenderRob/img/renderrob_icon.ico /home/ubuntu/RenderRob/src/rr_master.py
 
 
