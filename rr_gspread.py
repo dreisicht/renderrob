d1df7d01f877cedfacfa8709662aff8f6482d247
Peter Baintner <pb@dreisicht.net>
2023-05-15 23:34:47 +0200

    Refactor of gspread module

--- rr_gspread.py
+++ rr_gspread.py
@@ -1,67 +1,27 @@
 """ This module contains functions to interact with Google Sheets API."""
 import os
 import pickle
-import sys
-import webbrowser
-from typing import List, Tuple
 
-import util.rr_printing as rr_printing
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+
+from util import rr_printing
 
 SHEETNAME = 'Render Rob 2.0'
 TEMPLATE_SHEET_ID = "1prG29lIic0Qqc_fGq5sLdnDh1u8Nkd45MxdaCuRp1Rw"
 DOCS_URI = "https://docs.google.com/spreadsheets/d/"
-
-CLIENT_SECRET = {
-    "installed": {
-        "client_id": "243119286709-ceqv1581dilf28n3tiuspu8csj66tv7b.apps.googleusercontent.com",
-        "project_id": "renderpipeline",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret": "iG99SL4rYTUIZC-f9WtdrnKv",
-        "redirect_uris": [
-                    "urn:ietf:wg:oauth:2.0:oob",
-                    "http://localhost"
-        ]
-    }
-}
-
-# This access scope grants read-only access to the authenticated user's Drive
-# account.
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
-API_SERVICE_NAME = 'sheets'
-API_VERSION = 'v4'
+CREDENTIALS_PATH = "cache/credentials.json"
+TOKEN_PATH = "cache/token.pickle"
 
 
-def get_authenticated_service():
-  """Gets an authenticated service to access Google Drive API."""
-  token_path = "cache/token.pickle"
-  if os.path.exists(token_path):
-    with open(token_path, 'rb') as token:
-      creds = pickle.load(token)
-  # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-      if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-  else:
-    flow = InstalledAppFlow.from_client_config(
-        CLIENT_SECRET, ['https://www.googleapis.com/auth/drive.file'])
-    creds = flow.run_local_server()
-    with open(token_path, 'wb') as token:
-      pickle.dump(creds, token)
-
-  return build('sheets', 'v4', credentials=creds)
-
-
-def authorize_user():
+def authorize_user() -> any:
+  """Authorizes the user and returns the credentials."""
   creds = None
 
-  if os.path.exists(token_path):
-    with open(token_path, "rb") as token_file:
+  if os.path.exists(TOKEN_PATH):
+    with open(TOKEN_PATH, "rb") as token_file:
       creds = pickle.load(token_file)
 
   if not creds or not creds.valid:
@@ -69,17 +29,29 @@ def authorize_user():
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", scopes=scopes
+          CREDENTIALS_PATH, scopes=SCOPES
       )
       creds = flow.run_local_server(port=0)
-
-    with open(token_path, "wb") as token_file:
+    with open(TOKEN_PATH, "wb") as token_file:
       pickle.dump(creds, token_file)
 
   return creds
 
 
+def get_user_name(service) -> str:
+  """Returns the user name."""
+  about = service.about().get(fields='user').execute()
+  user = about.get('user', {})
+  return user.get('displayName', '').split(" ")[0]
+
+
+def rename_file(service, file_id, new_name) -> None:
+  """Renames the file."""
+  service.files().update(fileId=file_id, body={'name': new_name}).execute()
+
+
 def copy_spreadsheet(service, spreadsheet_id):
+  """Copies the spreadsheet and returns the ID."""
   copy_body = {
       'name': 'Copy of Public Spreadsheet'
   }
@@ -88,208 +60,45 @@ def copy_spreadsheet(service, spreadsheet_id):
   return response['id']
 
 
-def read_spreadsheet(service, spreadsheet_id):
-  sheet = service.spreadsheets()
-  result = sheet.values().get(spreadsheetId=spreadsheet_id, range='Sheet1!A1:C3').execute()
+def read_spreadsheet(service, spreadsheet_id, value_range):
+  """Reads the spreadsheet and returns the values."""
+  result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
+                                               majorDimension="ROWS",
+                                               range=value_range).execute()
   values = result.get('values', [])
   if not values:
-    print('No data found.')
+    rr_printing.print_error('No data found.')
   else:
-    print('Spreadsheet values:')
-    for row in values:
-      print(row)
+    return values
 
 
-# def create_sheet(service, sheet_name: str) -> str:
-#   """Creates a spreadsheet, returns the newly created spreadsheet ID."""
-#   spreadsheet = {
-#       'properties': {
-#           'title': sheet_name,
-#       }
-#   }
-#   spreadsheet = service.spreadsheets().create(body=spreadsheet,
-#                                               fields='spreadsheetId').execute()
-#   return spreadsheet.get('spreadsheetId')
-
-
-# def batch(service, spreadsheet_id, requests):
-#   """Executes multiple requests in a single batch."""
-#   body = {
-#       'requests': requests
-#   }
-#   return service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id, body=body).execute()
-
-
-# def rename_sheet(service, spreadsheet_id, sheet_id, new_name):
-#   """Renames a sheet."""
-#   return batch(service, spreadsheet_id, {
-#       "updateSheetProperties": {
-#           "properties": {
-#               "sheetId": sheet_id,
-#               "title": new_name,
-#           },
-#           "fields": "title",
-#       }
-#   })
-
-
-# def hide_sheet(service, spreadsheet_id, sheet_id):
-#   """Sets a sheet to be hidden."""
-#   return batch(service, spreadsheet_id, {
-#       "updateSheetProperties": {
-#           "properties": {
-#               'sheetId': sheet_id,
-#               'hidden': True
-#           },
-#           "fields": "title",
-#       }
-#   })
-
-
-# def delete_sheet(service, spreadsheet_id, sheet_id):
-#   """Deletes a sheet."""
-#   return batch(service, spreadsheet_id, {
-#       "deleteSheet": {
-#           "sheetId": sheet_id
-#       }
-#   }
-#   )
-
-
-# def get_sheets(service, spreadsheet_id):
-#   """Returns all sheets in a spreadsheet."""
-#   sheet_metadata = service.spreadsheets().get(
-#       spreadsheetId=spreadsheet_id).execute()
-#   return sheet_metadata.get('sheets', '')
-
-
-def cleanup_sheet(service, spreadsheet_id):
-  """Cleans up the sheet, removing all sheets except for the first one."""
-  sheets = get_sheets(service, spreadsheet_id)
-  for sheet in sheets:
-    title = sheet.get("properties", {}).get("title")
-    sheet_id = sheet.get("properties", {}).get("sheetId")
-    if sheet_id == 0 and len(sheets) == 1:
-      os.remove("cache/SHEETCACHE")
-      rr_printing.print_error(
-          "Your sheet is empty. Next time you launch me, I will start from the beginning. " +
-          DOCS_URI + spreadsheet_id)
-    if "jobs" in title and title != "jobs":
-      rename_sheet(service, spreadsheet_id, sheet_id, "jobs")
-      title = "jobs"
-    elif "globals" in title and title != "globals":
-      rename_sheet(service, spreadsheet_id, sheet_id, "globals")
-      title = "globals"
-    elif len(sheets) == 1:
-      os.remove("cache/SHEETCACHE")
-      rr_printing.print_error(
-          "Something went terribly wrong. I will create a new sheet if you launch me again.")
-    elif title not in ('jobs', 'globals'):
-      delete_sheet(service, spreadsheet_id, sheet_id)
-
-
-def read_from_sheetcache(sheet_name: str) -> str:
+def get_spreadsheet_id_or_create(drive_service: any) -> str:
   """Reads the sheet cache, checks if the sheet name is correct and returns the ID."""
-  with open("cache/SHEETCACHE", "r", encoding="UTF-8") as sheetcache:
-    sheetcache_content = sheetcache.read()
-  if sheetcache_content.split(";")[0] != sheet_name:
-    os.remove("cache/SHEETCACHE")
-    rr_printing.print_info(
-        "You changed the name of the sheet. Authentication is required again.")
-    raise FileNotFoundError
-  spreadsheet_id = sheetcache_content.split(";")[1]
-  rr_printing.print_info(f"I'm using this cached spreadsheet: {DOCS_URI + spreadsheet_id}")
-  return spreadsheet_id
-
-
-def sheet_setup_process(sheet_name: str = SHEETNAME) -> str:
-  """Full process to setup the access as well as the sheet in the users google drive."""
-  authenticated_access = get_authenticated_service()
-  try:
-    read_from_sheetcache(sheet_name)
-  except FileNotFoundError:
-    spreadsheet_id = create_sheet(authenticated_access, sheet_name)
-    # Open the template sheet in the browser. Not the newly created one!
-    webbrowser.open(
-        DOCS_URI + TEMPLATE_SHEET_ID)
-    with open("cache/SHEETCACHE", "x", encoding="UTF-8") as sheetcache:
-      sheetcache.write(sheet_name + ";" + spreadsheet_id)
-    rr_printing.print_info_input(
-        "I Created a new Render Rob Spreadsheet. Please copy both 'jobs' and 'globals"
-        "sheet from the template to the newly created document. If you copied them you can close"
-        "the tab and")
-    for _ in range(7):
-      sheets = get_sheets(authenticated_access, spreadsheet_id)
-      jobs_c = 0
-      globals_c = 0
-      for sheet in sheets:
-        if "jobs" in sheet.get("properties", {}).get("title"):
-          jobs_c += 1
-          if jobs_c > 1:
-            delete_sheet(authenticated_access, spreadsheet_id,
-                         sheet.get("properties", {}).get("sheetId"))
-            jobs_c = jobs_c - 1
-        if "globals" in sheet.get("properties", {}).get("title"):
-          globals_c += 1
-          if globals_c > 1:
-            delete_sheet(authenticated_access, spreadsheet_id,
-                         sheet.get("properties", {}).get("sheetId"))
-            globals_c = globals_c - 1
-      if jobs_c != 1 or globals_c != 1:
-        rr_printing.print_warning(
-            f"I'm sorry, but you did not copy both sheets to the new spreadsheet. "
-            f"Please try again! If you want to see the sheet, it's here: "
-            f"{DOCS_URI + spreadsheet_id}")
-      else:
-        break
-
-    else:
-      rr_printing.print_error(
-          "To be honest, we were never supposed to land here. Please try Running Render Rob again!")
-      return None
-    cleanup_sheet(authenticated_access, spreadsheet_id)
-    webbrowser.open(DOCS_URI + spreadsheet_id)
-    rr_printing.print_info_input(
-        "I opened the spreadsheet and I'll let you fill it out. If you're done")
-  return spreadsheet_id
-
-
-def get_values(service, spreadsheet_id, value_range):
-  """Gets values from a spreadsheet."""
-  for _ in range(2):
-    try:
-      result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
-                                                   majorDimension="ROWS",
-                                                   range=value_range).execute()
-    except HttpError as http_error:
-      if "Unable to parse range" in http_error.reason:
-        cleanup_sheet(service, spreadsheet_id)
-      if "not found" in http_error.reason:
-        os.remove("cache/SHEETCACHE")
-        rr_printing.print_error(
-            "I couldn't find the sheet. Are you sure you didn't delete it?")
-    else:
-      break
+  if not os.path.exists("cache/SHEETCACHE"):
+    rr_printing.print_info("No sheet cache found. Creating a new one.")
+    spreadsheet_id = copy_spreadsheet(drive_service, TEMPLATE_SHEET_ID)
+    rename_file(drive_service, spreadsheet_id, SHEETNAME + " " + get_user_name(drive_service))
+    with open("cache/SHEETCACHE", "w", encoding="UTF-8") as sheetcache:
+      sheetcache.write(spreadsheet_id)
   else:
-    print("Please check your sheet, if it contains a globals and a jobs sheet: " +
-          DOCS_URI + spreadsheet_id)
-    sys.exit()
+    with open("cache/SHEETCACHE", "r", encoding="UTF-8") as sheetcache:
+      spreadsheet_id = sheetcache.read()
+    rr_printing.print_info(f"I'm using this cached spreadsheet: {DOCS_URI + spreadsheet_id}")
 
-  values = result.get('values', [])
-  return values
+  return spreadsheet_id
 
 
-def query_sheet(spreadsheet_id: str) -> Tuple[List[List[str]], List[List[str]]]:
-  """Queries the sheet for jobs and globals."""
-  authenticated_access = get_authenticated_service()
-  jobs_values = get_values(authenticated_access, spreadsheet_id, "jobs!A1:V81")
-  globals_values = get_values(authenticated_access, spreadsheet_id, "globals!A1:C7")
+def download_spreadsheet_content():
+  """Downloads the spreadsheet content and returns the values."""
+  creds = authorize_user()
+  drive_service = build('drive', 'v3', credentials=creds)
+  rr_printing.print_info(f"Hello {get_user_name(drive_service)}!")
+  spreadsheet_id = get_spreadsheet_id_or_create(drive_service)
+  spreadsheet_service = build("sheets", "v4", credentials=creds)
+  jobs_values = read_spreadsheet(spreadsheet_service, spreadsheet_id, "jobs!A1:V81")
+  globals_values = read_spreadsheet(spreadsheet_service, spreadsheet_id, "globals!A1:C7")
   return jobs_values, globals_values
 
 
-def main():
-  creds = authorize_user()
-  service = build('sheets', 'v4', credentials=creds)
-  copied_spreadsheet_id = copy_spreadsheet(service, spreadsheet_id)
-  drive_service = build('drive', 'v3', credentials=creds)
-  read_spreadsheet(service, copied_spreadsheet_id)
+if __name__ == "__main__":
+  print(download_spreadsheet_content())
--- rr_gspread_test.py
+++ rr_gspread_test.py
@@ -1,8 +1,7 @@
 """Tests the functionalities in the GSpread."""
-import os
 from unittest import TestCase
 
-import googleapiclient
+from google.oauth2.credentials import Credentials
 
 import rr_gspread
 
@@ -10,22 +9,22 @@ import rr_gspread
 class TestGspread(TestCase):
   """Tests the functionalities in the GSpread."""
 
-  def setUp(self) -> None:
-    os.chdir("src")
-
-    return super().setUp()
-
-  def test_get_authenticated_service(self) -> None:
-    """Tests the get_authenticated_service function."""
-    a = rr_gspread.get_authenticated_service()
-    self.assertEqual(type(rr_gspread.get_authenticated_service()), googleapiclient.discovery.Resource)
+  def test_authorize_user(self) -> None:
+    """Tests the authorize_user function."""
+    self.assertEqual(type(rr_gspread.authorize_user()), Credentials)
 
   def test_query_sheet(self) -> None:
     """Tests the query_sheet function."""
     expected_result = ([['', 'active', '.blend file path', 'active camera', 'start\nframe', 'end\nframe', 'X\nres', 'Y\nres', 'samples', 'file\nformat', 'Cycles\n(Eevee)', 'CPU', 'GPU', 'motion\nblur', 'new\nversion', 'high\nquality', 'anima-\ntion\ndenoise', 'denoise', 'scene', 'view layer', 'comments'],
                         ['', 'TRUE', 'file.blend', 'Camera', '1', '250', '1920', '1080', '16', '', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE',
-                         'TRUE', 'FALSE', 'FALSE', '', '', '', 'TRUEfile.blendCamera12501920108016TRUETRUETRUETRUETRUETRUEFALSEFALSE'],
-                        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '#REF!']],
+                         'TRUE', 'FALSE', 'FALSE', '', '', '', 'TRUEfile.blendCamera12501920108016exrTRUETRUETRUETRUETRUETRUEFALSEFALSE'],
+                        ['', 'FALSE', 'file.blend', 'Camera', '1', '250', '1920', '1080', '16', 'exr', 'TRUE',
+                         'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'FALSE', 'FALSE', '', '', '', '#REF!'],
+                        ['', 'FALSE', 'file.blend', 'Camera', '1', '250', '1920', '1080', '16', 'exr', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'FALSE', 'FALSE', '', '', '',
+                         'FALSEfile.blendCamera12501920108016exrTRUETRUETRUETRUETRUETRUEFALSEFALSE'],
+                        ['', 'FALSE', 'file.blend', 'Camera', '1', '250', '1920', '1080', '16', 'exr', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE',
+                         'TRUE', 'FALSE', 'FALSE', '', '', '', 'FALSEfile.blendCamera12501920108016exrTRUETRUETRUETRUETRUETRUEFALSEFALSE'],
+                        ['', 'FALSE', 'file.blend', 'Camera', '1', '250', '1920', '1080', '16', 'exr', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'FALSE', 'FALSE', '', '', '', 'FALSEfile.blendCamera12501920108016exrTRUETRUETRUETRUETRUETRUEFALSEFALSE']],
                        [['path to blender', 'C:\\Program Files (x86)\\Blender Foundation\\Blender\\blender.exe'],
                         ['path to render output folder', 'C:\\Path\\To\\My\\Renders'],
                         ['path to blender files folder', '"C:\\Users\\peter\\Desktop\\opt"'],
@@ -34,4 +33,4 @@ class TestGspread(TestCase):
                         ['preview n-th frames', '24', 'FALSE'],
                         ['addons to activate']])
     self.assertEqual(expected_result,
-                     rr_gspread.query_sheet(spreadsheet_id="1RK9LehhMt0vEDU9_usjELTvF397WePwqncWAU5hL94A"))
+                     rr_gspread.download_spreadsheet_content())
