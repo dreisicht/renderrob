39ff77e0a289fd910ba371ea636481c75c397886
Peter Baintner <pb@dreisicht.net>
2023-08-16 08:14:23 +0200

    Fixed few issues with testing.

---
+++ .github/workflows/install.sh
@@ -0,0 +1,4 @@
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+sudo apt install protobuf-compiler
+sudo apt install libgl1
--- render_settings_setter.py
+++ render_settings_setter.py
@@ -2,7 +2,7 @@
 
 from typing import List
 
-import bpy  # pylint: disable=import-error
+import bpy
 
 from utils import print_utils, rr_c_image
 
