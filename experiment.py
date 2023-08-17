7dc5215bf1d8712ac65f2a7092919fe902f91d77
Peter Baintner <pb@dreisicht.net>
2023-08-17 08:28:30 +0200

    try

--- experiment.py
+++ experiment.py
@@ -1,7 +1,19 @@
 """Experiment with the Blender API."""
 
 import bpy
+from proto import state_pb2
+from PySide6.QtWidgets import QApplication
+import unittest
 
-print(bpy.context.scene.name)
-bpy.data.scenes.new("Scene")
-print(bpy.context.scene.name)
+
+class TestBasic(unittest.TestCase):
+  """Test the basic functionality of the Blender API."""
+
+  def test_basic(self):
+    """Test the basic functionality of the Blender API."""
+    print(bpy.context.scene.name)
+    bpy.data.scenes.new("Scene")
+    print(bpy.context.scene.name)
+    print(state_pb2)
+    print(QApplication)
+    print("Everything is fine.")
