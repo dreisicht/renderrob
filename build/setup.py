b770e89c6aa2aa7ade3fd77f7efe449420b52ffc
Peter Baintner <pb@dreisicht.net>
2024-03-28 17:09:24 +0100

    pylint

--- build/setup.py
+++ build/setup.py
@@ -1,20 +1,28 @@
+"""Setup for cx_freeze."""
+
 import sys
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {
-    "excludes": ["tkinter", "unittest"],
-    "zip_include_packages": ["encodings", "PySide6", "protos"],
-}
 
-# base="Win32GUI" should be used only for Windows GUI app
-base = "Win32GUI" if sys.platform == "win32" else None
+def main():
+  """Setup for cx_freeze."""
+  # Dependencies are automatically detected, but it might need fine tuning.
+  build_exe_options = {
+      "excludes": ["tkinter", "unittest"],
+      "zip_include_packages": ["encodings", "PySide6", "protos"],
+  }
 
-icon = "icons/icon.ico"
-setup(
-    name="renderrob",
-    version="0.1",
-    description="My GUI application!",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("renderrob.py", base=base, icon=icon)],
-)
+  # base="Win32GUI" should be used only for Windows GUI app
+  base = "Win32GUI" if sys.platform == "win32" else None
+
+  icon = "icons/icon.ico"
+  setup(
+      name="renderrob",
+      version="0.1",
+      description="My GUI application!",
+      options={"build_exe": build_exe_options},
+      executables=[Executable("renderrob.py", base=base, icon=icon)],
+  )
+
+
+main()
