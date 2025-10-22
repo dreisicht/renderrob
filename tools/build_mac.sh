2524e749b26eabc45ed20f7d8aa4f0b106a880c1
Peter Baintner <git@dreisicht.net>
2025-10-22 15:12:39 +0200

    Proper dmg layout

--- tools/build_mac.sh
+++ tools/build_mac.sh
@@ -1,4 +1,5 @@
-rm -rf build dist renderrob.app renderrob.zip Renderrob.dmg
+#!/bin/sh
+rm -rf build dist renderrob.app renderrob.zip renderrob.dmg
 uv run setup.py py2app
 
 uv run tools/sign_all.py
@@ -10,11 +11,21 @@ uv run appNotarize -a renderrob --verbose
 uv run appStaple -d renderrob -a renderrob --verbose
 uv run appVerify -d renderrob -a renderrob
 
-# ditto -c -k --keepParent "dist/renderrob.app" "renderrob.zip"
+test -f renderrob.dmg && rm renderrob.dmg
+create-dmg \
+  --volname "Render Rob Installer" \
+  --volicon "src/icon/icon.icns" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "Renderrob.app" 200 190 \
+  --hide-extension "Renderrob.app" \
+  --app-drop-link 600 185 \
+  "renderrob.dmg" \
+  "dist/renderrob.app/"
 
-hdiutil create -fs HFS+ -srcfolder "dist/renderrob.app" -volname "Renderrob Installer" "Renderrob.dmg"
+codesign -f --timestamp -s 5K24F4J2M5 renderrob.dmg
+xcrun notarytool submit "renderrob.dmg" --keychain-profile "dev" --wait
+xcrun stapler staple "renderrob.dmg"
+xcrun stapler validate "renderrob.dmg"
 
-codesign -f --timestamp -s 5K24F4J2M5 Renderrob.dmg
-xcrun notarytool submit "Renderrob.dmg" --keychain-profile "dev" --wait
-xcrun stapler staple "Renderrob.dmg"
-xcrun stapler validate "Renderrob.dmg"
